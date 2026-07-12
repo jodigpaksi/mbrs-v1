8dbc778acb0814eb0bdcfb0e082ef63ba6d51cd0
jodigpaksi <jodiginandrapaksi@gmail.com>
2026-07-12 18:07:08 +0700

    Revert "Update cors.php"
    This reverts commit 59601fc087f67461440843d5a6886a54e5a6ce39.

--- server/config/cors.php
+++ server/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:4173'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
